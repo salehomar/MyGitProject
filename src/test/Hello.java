40e3723ee938d5ded904f3a5e8d41690f2462ed4
salehomar <salehabp@hotmail.com>
2018-03-06 23:24:21 -0500

    3/7

--- src/test/Hello.java
+++ src/test/Hello.java
@@ -4,6 +4,7 @@ public class Hello {
 	public static void main(String[] args) {
 		System.out.println("Can't wait to finsih");
 		System.out.println("I am still here");
+		System.out.println("are you here");
 	}
 
 }
